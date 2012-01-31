a9f6a97aeaebdcdb4967e5c778a2454fda448e43
boncagni <luca.boncagni@enea.it>
2012-01-31 15:43:23 +0000

    Adjust Makefile for RTAIConsole and rollback global_obj_link.ld

--- BaseLib2/rtai/RTAIBaseLib2.mod.c
+++ BaseLib2/rtai/RTAIBaseLib2.mod.c
@@ -1,27 +1,3 @@
-/*
- * Copyright 2011 EFDA | European Fusion Development Agreement
- *
- * Licensed under the EUPL, Version 1.1 or - as soon they 
-   will be approved by the European Commission - subsequent  
-   versions of the EUPL (the "Licence"); 
- * You may not use this work except in compliance with the 
-   Licence. 
- * You may obtain a copy of the Licence at: 
- *  
- * http://ec.europa.eu/idabc/eupl
- *
- * Unless required by applicable law or agreed to in 
-   writing, software distributed under the Licence is 
-   distributed on an "AS IS" basis, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
-   express or implied. 
- * See the Licence for the specific language governing 
-   permissions and limitations under the Licence. 
- *
- * $Id$
- *
-**/
-
 #include <linux/module.h>
 #include <linux/vermagic.h>
 #include <linux/compiler.h>
@@ -39,9 +15,7 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 };
 
 static const char __module_depends[]
-__used
+__attribute_used__
 __attribute__((section(".modinfo"))) =
-"depends=";
+"depends=RTAIBaseLib2";
 
-
-MODULE_INFO(srcversion, "896232E3472F7D3DC2D5CAF");
