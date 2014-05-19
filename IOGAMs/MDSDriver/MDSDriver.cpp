4c795801dcd2f70db9290c0fa967b6514e05eae9
gmanduchi <gabriele.manduchi@igi.cnr.it>
2014-05-19 08:02:31 +0000

    MDS Driver for reading refeernce waveforms and writing signals

---
+++ IOGAMs/MDSDriver/MDSDriver.cpp
@@ -0,0 +1,231 @@
+/*
+ * Gabriele Manduchi
+ * 2014
+ */
+
+
+// private includes
+#include "MDSDriver.h"
+
+// framework include to use GODBFindByName
+#include "GlobalObjectDataBase.h"
+
+// framework includes
+#include "CDBExtended.h"
+#include "MDSInterface.h"
+#include <time.h>
+
+
+static char *css = "table.bltable {"
+         "margin: 1em 1em 1em 2em;"
+         "background: whitesmoke;"
+         "border-collapse: collapse;"
+      "}"
+      "table.bltable th, table.bltable td {"
+          "border: 1px silver solid;"
+          "padding: 0.2em;"
+      "}"
+      "table.bltable th {"
+          "background: gainsboro;"
+          "text-align: left;"
+      "}"
+      "table.bltable caption {"
+          "margin-left: inherit;"
+          "margin-right: inherit;"
+      "}";
+
+
+    /** ObjectLoadSetup 
+     * Configuration parameters are:
+     * Name - name of the pulse file
+     * Shot - shot number of the pulse file
+     * NumberOfInputs - number of nodes containing input data
+     * FirstNodePath - path name of the first node of the list
+     */
+     bool MDSDriver::ObjectLoadSetup(ConfigurationDataBase &info,StreamInterface *err)
+     {
+    	CDBExtended cdb(info);
+	//Free previously allocated stuff (if any)
+	//freeData();
+    // reads NumberOfInputs and NumberOfOutputs
+    // (both must be set, take a look at GenericAcqModule)
+        if ( !GenericAcqModule::ObjectLoadSetup(info, NULL) ) 
+	{
+            AssertErrorCondition(FatalError,
+        		"MDSDriver::ObjectLoadSetup: %s GenericAcqModule wrong or missing info", Name() );
+            return False;
+    	}
+
+	//Output signals: time(32 bits), trigger(32 bits), signals (integer, 32 bits) 
+	numRefWaveforms = NumberOfInputs();
+	if(numRefWaveforms > 0)
+	{
+	    BString *refNames = new BString[numRefWaveforms];
+	    int retNumRefs = numRefWaveforms;
+	    if(!cdb.ReadBStringArray(refNames, &retNumRefs, 1, "ReferenceWaveNames"))
+	    {
+   	    	AssertErrorCondition(FatalError,
+    			"MDSDriver::ObjectLoadSetup: %s ReferenceWaveNames not set", Name() );
+    	    	return False;
+	    }
+	    if(retNumRefs != numRefWaveforms)
+	    {
+    	    	AssertErrorCondition(FatalError,
+    			"MDSDriver::ObjectLoadSetup: %s the number %d of ReferenceWaveNames is different from the number %d of input signals", Name(), retNumRefs, numRefWaveforms );
+    	    	return False;
+	    }
+	    refWaveformNames = new char *[numRefWaveforms];
+	    for(int i = 0; i < numRefWaveforms; i++)
+	    {
+		char *currName = (char *)refNames[i].Buffer();
+		refWaveformNames[i] = new char[strlen(currName) + 1];
+		strcpy(refWaveformNames[i], currName);
+		printf("%s\n", currName);
+	    }
+	    refWaveformIdxs = new int[numRefWaveforms];
+	    currInputs = new float[numRefWaveforms];
+	}
+	numOutSignals = NumberOfOutputs();
+	if(numOutSignals > 0)
+	{
+	    BString outNames[numOutSignals];
+	    int retNumOuts =  numOutSignals;
+	    if(!cdb.ReadBStringArray(outNames, &retNumOuts, 1, "OutSignalNames"))
+	    {
+    	    	AssertErrorCondition(FatalError,
+    			"MDSDriver::ObjectLoadSetup: %s OutSignalNames not set", Name() );
+    	    	return False;
+	    }
+	    if(retNumOuts != numOutSignals)
+	    {
+    	    	AssertErrorCondition(FatalError,
+    			"MDSDriver::ObjectLoadSetup: %s the number %d of OutSignalNames is different from the number %d of output signals", Name(), retNumOuts, numOutSignals );
+    	    	return False;
+	    }
+	    outSignalNames = new char *[numOutSignals];
+	    for(int i = 0; i < numOutSignals; i++)
+	    {
+		char *currName = (char *)outNames[i].Buffer();
+		outSignalNames[i] = new char[strlen(currName) + 1];
+		strcpy(outSignalNames[i], currName);
+		printf("%s\n", currName);
+	    }
+	    outSignalIdxs = new int[numOutSignals];
+	    currOutputs = new float[numOutSignals];
+	//Read Optional Descriptions
+	    outSignalDescriptions = new char *[numOutSignals];
+	    BString outSignalDescs[numOutSignals];
+	    if(cdb.ReadBStringArray(outSignalDescs, &retNumOuts, numOutSignals, "OutSignalDescriptions"))
+	    {
+	    	if(retNumOuts != numOutSignals)
+	    	{
+    	    	    AssertErrorCondition(FatalError,
+    			"MDSDriver::ObjectLoadSetup: %s the number %d of OutSignalDescriptions is different from the number %d of input signals", Name(), retNumOuts, numOutSignals );
+    	    	    return False;
+	    	}
+		for(int i = 0; i < numOutSignals; i++)
+		{
+		    char *currName = (char *)outSignalDescs[i].Buffer();
+		    outSignalDescriptions[i] = new char[strlen(currName) + 1];
+		    strcpy(outSignalDescriptions[i], currName);
+		    printf("%s\n", currName);
+		}
+	    }
+	    else
+	    {
+		for(int i = 0; i < numOutSignals; i++)
+		    outSignalDescriptions[i] = 0;
+	    }
+	}
+        if( !cdb.ReadInt32(deviceIdx, "MdsId") ) 
+	{
+    	    AssertErrorCondition(FatalError,
+    			"MDSDriver::ObjectLoadSetup: %s MdsId is not declared", Name() );
+    	    return False;
+    	}
+	return true;
+    }
+
+    bool MDSDriver::PulseStart()
+    {
+	for(int i = 0; i < numRefWaveforms; i++)
+	{
+	    if((refWaveformIdxs[i] = MDSInterface::prepareWaveParameterReadout(deviceIdx, refWaveformNames[i])) == -1)
+ 	    {
+    	    	AssertErrorCondition(FatalError,
+    			"MDSDriver::PulseStart: %s Reference Waveform %s is not declared in the target MDS device", Name(),  refWaveformNames[i]);
+    	    	return False;
+    	    }
+	}
+	MDSInterface::resetSignals(deviceIdx);
+ 	for(int i = 0; i < numOutSignals; i++)
+	{
+            if((outSignalIdxs[i] = MDSInterface::declareSignal(deviceIdx, outSignalNames[i], outSignalDescriptions[i])) == -1)
+ 	    {
+    	    	AssertErrorCondition(FatalError,
+    			"MDSDriver::PulseStart: %s Error declaring out signal %s for device idx %d", Name(),  outSignalNames[i], deviceIdx);
+    	    	return False;
+    	    }
+	}
+	return true;
+    }
+  
+    bool MDSDriver::WriteData(uint32 usecTime, const int32 *buffer)
+    {
+	float time = usecTime * 1E-6;
+//	printf("WRITE SIGNALS: %d %d", usecTime, numOutSignals);
+	for(int i = 0; i < numOutSignals; i++)
+	{
+	    currOutputs[i] = ((float *)buffer)[i];
+	    MDSInterface::reportSignal(deviceIdx, outSignalIdxs[i], ((float *)buffer)[i], time);
+
+//	    printf("%f ", currOutputs[i]);
+	}
+//	printf("\n");
+	return true;
+    }
+
+    int32 MDSDriver::GetData(uint32 usecTime, int32 *buffer, int32 bufferNumber)
+    {
+	double time = usecTime * 1E-6;
+//	printf("GET WAVES: \n");
+	for(int i = 0; i < numRefWaveforms; i++)
+	{
+	    currInputs[i] = ((float *)buffer)[i] = (float)MDSInterface::getWaveParameter(deviceIdx, refWaveformIdxs[i], time);
+
+//	    printf("%f ", currInputs[i]);
+	}
+//	printf("\n");
+	return numRefWaveforms * sizeof(float);
+    }
+    int64 MDSDriver::GetUsecTime(){return HRTRead64();}
+
+    bool MDSDriver::ProcessHttpMessage(HttpStream &hStream) 
+    {
+    	hStream.SSPrintf("OutputHttpOtions.Content-Type","text/html");
+    	hStream.keepAlive = False;
+
+    	hStream.Printf("<html><head><title>%s</title>", Name());
+    	hStream.Printf( "<style type=\"text/css\">\n" );
+    	hStream.Printf("%s\n", css);
+    	hStream.Printf( "</style></head><body>\n" );
+    	hStream.Printf("Device Id: %d<br>", deviceIdx);
+    	hStream.Printf("<table class=\"bltable\">\n");
+    	hStream.Printf("<tr><td> Input Name </td> <td>Current Input Value</td></tr>\n");
+	for(int i = 0; i < numRefWaveforms; i++)
+	    hStream.Printf("<tr><td>%s</td><td>%f</td></tr>\n", refWaveformNames[i], currInputs[i]);
+	hStream.Printf("</table><br><br>\n");
+
+   	hStream.Printf("<table class=\"bltable\">\n");
+    	hStream.Printf("<tr><td> Output Name </td> <td>Current Output Value</td></tr>\n");
+	for(int i = 0; i < numOutSignals; i++)
+	    hStream.Printf("<tr><td>%s</td><td>%f</td></tr>\n", outSignalNames[i], currOutputs[i]);
+	hStream.Printf("</table><br><br>\n");
+
+    	hStream.Printf("</body></html>");
+    	hStream.WriteReplyHeader(True);
+    	return True;
+    }
+
+
+OBJECTLOADREGISTER(MDSDriver, "$Id: MDSDriver.cpp,v 1.0 2014/04/16 12:12:12 gab Exp $")
---
+++ IOGAMs/MDSDriver/MDSDriver.h
@@ -0,0 +1,142 @@
+
+#ifndef MDSDRIVER_H_
+#define MDSDRIVER_H_
+
+// framework includes
+#include "System.h"
+#include "MutexSem.h"
+#include "GenericAcqModule.h"
+#include "MDSInterface.h"
+
+// framework object registration macro
+OBJECT_DLL(MDSDriver)
+
+class MDSDriver: public GenericAcqModule 
+{
+// framework object registration macro
+private:
+    int deviceIdx;
+// Reference Waveform settings
+    char **refWaveformNames;
+    int numRefWaveforms;
+    int *refWaveformIdxs;
+//Output Signals settings
+    char **outSignalNames;
+    char **outSignalDescriptions;
+    int numOutSignals;
+    int *outSignalIdxs; 
+    float *currInputs;
+    float *currOutputs;
+    void freeData()
+    {
+	if(numRefWaveforms > 0)
+	{
+	    for(int i = 0; i < numRefWaveforms; i++)
+		delete [] refWaveformNames[i];
+	    delete [] refWaveformNames;
+	    delete [] refWaveformIdxs;
+	    delete [] currInputs;
+	}
+	if(numOutSignals > 0)
+	{
+	    for(int i = 0; i < numOutSignals; i++)
+	    {
+		delete [] outSignalNames[i];
+		if(outSignalDescriptions)
+		    delete [] outSignalDescriptions[i];
+	    }
+	    delete []outSignalNames;
+	    delete [] outSignalIdxs;
+	    delete [] currOutputs;
+	}
+    }
+    
+public:
+    /** Constructor */
+    MDSDriver()
+    {
+	numRefWaveforms = numOutSignals = 0;
+	deviceIdx = -1;
+    }
+    /** Destructor */
+    ~MDSDriver()
+    {
+	freeData();
+    }
+
+    /** ObjectLoadSetup 
+     * Configuration parameters are:
+     * DeviceIdx: index of the target MDSplus device
+     * Reference waveform name array: names of the reference waveforms. Must fit the declared waveforms in the device
+     * Out Signal name array: output signal names. This is dynamic and not declared in advance in the pulse file.
+     */
+    virtual bool ObjectLoadSetup(ConfigurationDataBase &info,StreamInterface *err);
+    /** Save parameter */
+    virtual bool ObjectDescription(StreamInterface &s, bool full=False, StreamInterface *err=NULL)
+    { return True; }
+    // ------------------------------------------------------------------------
+
+    
+    /** Set board used as input */
+    virtual bool SetInputBoardInUse(bool on = True)
+    {
+	if(on) printf("SET INPUT\n");
+        if(inputBoardInUse && on) {
+            AssertErrorCondition(InitialisationError,
+            		"MDSDriver::SetInputBoardInUse: Board %s is already in use", Name());
+            return False;
+        }
+        inputBoardInUse  = on;
+        return True;
+    }
+    /** Set board used as output */
+    virtual bool SetOutputBoardInUse(bool on = True)
+    {
+	if(on) printf("SET OUTPUT\n");
+        if(outputBoardInUse && on) 
+	{
+            AssertErrorCondition(FatalError,
+            		"MDSDriver::SetOutputBoardInUse: Board %s can only be set as output", Name());
+            return False;
+	}
+	return True;
+    }
+    // ------------------------------------------------------------------------
+
+    
+    virtual bool WriteData(uint32 usecTime, const int32 *buffer);
+
+    /** GetData
+     * Copy local input buffer in destination buffer:
+     * return  0 if data not ready
+     * return <0 if error
+     * return >0 if OK (returns the number of bytes copied)
+     * DataFormat: Time, trigger (always 1), samples
+     */
+    virtual int32 GetData(uint32 usecTime, int32 *buffer, int32 bufferNumber = 0);
+     /**
+      * Polling method
+      * Will returns true when data are available. Blocks when data is finished
+      */
+    virtual bool Poll(){return True;}
+    virtual bool EnableAcquisition(){ printf("ENABLE ACQUISITION\n"); return True;}
+
+    virtual bool DisableAcquisition(){ printf("ENABLE ACQUISITION\n"); return True;}
+
+    /** return current time in mucroseconds */
+    int64 GetUsecTime();
+    
+     /**
+      * Output an HTML page with the current setting
+      */
+     virtual bool ProcessHttpMessage(HttpStream &hStream);
+     /**
+      */
+     virtual bool PulseStart();
+private:     
+     OBJECT_DLL_STUFF(MDSDriver)
+
+};
+
+
+#endif /*MDSDRIVER_H_*/
