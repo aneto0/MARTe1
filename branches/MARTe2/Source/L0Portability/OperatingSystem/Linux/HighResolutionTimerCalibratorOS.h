598784208197f70a93ae2cc705bddd963b93b443
aneto <aneto@428a0d1b-0fa9-4229-b4c5-a2ab7cbf0508>
2015-02-05 22:49:03 +0000

    Work in progress. Need to finish ProcessorA.h

---
+++ branches/MARTe2/Source/L0Portability/OperatingSystem/Linux/HighResolutionTimerCalibratorOS.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2011 EFDA | European Fusion Development Agreement
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they 
+   will be approved by the European Commission - subsequent  
+   versions of the EUPL (the "Licence"); 
+ * You may not use this work except in compliance with the 
+   Licence. 
+ * You may obtain a copy of the Licence at: 
+ *  
+ * http://ec.europa.eu/idabc/eupl
+ *
+ * Unless required by applicable law or agreed to in 
+   writing, software distributed under the Licence is 
+   distributed on an "AS IS" basis, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
+   express or implied. 
+ * See the Licence for the specific language governing 
+   permissions and limitations under the Licence. 
+ *
+ * $Id: Processor.h 43 2012-02-08 17:07:58Z astephen $
+ *
+**/
+
+/**
+ * @file
+ * Access processor's information
+ */
+#ifndef HIGH_RESOLUTION_TIMER_CALIBRATOR_OS_H
+#define HIGH_RESOLUTION_TIMER_CALIBRATOR_OS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define LINUX_CPUINFO_BUFFER_SIZE 1023
+
+class HighResolutionTimerCalibratorOS {
+
+public:
+    uint32 HRTmSecTics;
+    uint64 HRTFrequency;
+    double HRTPeriod;
+
+    HighResolutionTimerCalibratorOS(){
+        HRTFrequency = 0;
+        HRTPeriod    = 0;
+
+        char buffer[LINUX_CPUINFO_BUFFER_SIZE + 1];
+
+        FILE *f;
+        f=fopen("/proc/cpuinfo","r");
+        uint32 size = LINUX_CPUINFO_BUFFER_SIZE;
+        size = fread(buffer,size,1,f);
+        fclose(f);
+
+        const char *pattern = "MHz";
+        char *p = strstr(buffer,pattern);
+        if (p != NULL){
+            p = strstr(p,":");
+            p++;
+            double f = atof(p);
+            if(f != 0){
+                f *= 1.0e6;
+                HRTFrequency = (int64)f;
+                HRTPeriod    = 1.0 / f;
+            }
+        }
+        HRTmSecTics = HRTFrequency / 1000;
+    }
+};
+
+#endif
+
