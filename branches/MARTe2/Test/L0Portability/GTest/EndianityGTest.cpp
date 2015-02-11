3e8ffa9fb8672a6270e42e13c3655106448c7f38
jtarrago <jtarrago@428a0d1b-0fa9-4229-b4c5-a2ab7cbf0508>
2015-02-11 14:55:47 +0000

    First GoogleTest test: Endianity

---
+++ branches/MARTe2/Test/L0Portability/GTest/EndianityGTest.cpp
@@ -0,0 +1,67 @@
+/*
+ *  Shapes_Test.cpp
+ *
+ *  Created on: Feb 5, 2015
+ */
+#include <limits.h>
+#include "gtest/gtest.h"
+#include "../EndianityTest.h"
+
+
+class EndianityGTest : public ::testing::Test {
+protected:
+	virtual void SetUp() {
+		// Code here will be called immediately after the constructor 
+		// (right before each test).
+
+		RecordProperty("Endianity", Endianity::Type());
+	}
+
+	virtual void TearDown() {
+		// Code here will be called immediately after each test
+		// (right before the destructor).
+	}
+};
+
+
+TEST_F(EndianityGTest,uint16Test){
+	EndianityTest<uint16> uint16Test(0xAABB);
+	ASSERT_TRUE(uint16Test.All());
+}
+
+TEST_F(EndianityGTest,int16Test){
+	EndianityTest<int16> int16Test(0xAABB);
+	ASSERT_TRUE(int16Test.All());
+}
+
+TEST_F(EndianityGTest,uint32Test){
+	EndianityTest<uint32> uint32Test(0xFFEEDDCC);
+	ASSERT_TRUE(uint32Test.All());
+}
+
+TEST_F(EndianityGTest,int32Test){
+	EndianityTest<int32> int32Test(0xAABBCCDD);
+	ASSERT_TRUE(int32Test.All());
+}
+
+TEST_F(EndianityGTest,uint64Test){
+	EndianityTest<uint64> uint64Test(0xFFEEDDCCBBAA1122);
+	ASSERT_TRUE(uint64Test.All());
+}
+
+TEST_F(EndianityGTest,int64Test){
+	EndianityTest<int64> int64Test(0xAABBCCDDEEFF3344);
+	ASSERT_TRUE(int64Test.All());
+}
+
+TEST_F(EndianityGTest,floatTest){
+	EndianityTest<float> floatTest(0.12345f);
+	ASSERT_TRUE(floatTest.All());
+}
+
+TEST_F(EndianityGTest,doubleTest){
+	EndianityTest<double> doubleTest(0.123456789);
+	ASSERT_TRUE(doubleTest.All());
+}
+
+
