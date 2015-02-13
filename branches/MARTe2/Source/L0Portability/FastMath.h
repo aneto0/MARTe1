5b96a798fd1d185a84c6313512831d545e5ae834
rvitelli <rvitelli@428a0d1b-0fa9-4229-b4c5-a2ab7cbf0508>
2015-02-13 11:29:01 +0000

    Fixed initialisation of constants in FastMath.h

--- branches/MARTe2/Source/L0Portability/FastMath.h
+++ branches/MARTe2/Source/L0Portability/FastMath.h
@@ -38,31 +38,31 @@ class FastMath {
 public:
 	// Math constants
 	/** e */
-	static const double E;
+	static const double E         = 2.7182818284590452354;
 	/** log2(e) */
-	static const double LOG2E;
+	static const double LOG2E     = 1.4426950408889634074;
 	/** log10(e) */
-	static const double LOG10E;
+	static const double LOG10E    = 0.43429448190325182765;
 	/** log(2) */
-	static const double LN2;
+	static const double LN2       = 0.69314718055994530942;
 	/** log(10) */
-	static const double LN10;
+	static const double LN10      = 2.30258509299404568402;
 	/** pi */
-	static const double PI;
+	static const double PI        = 3.14159265358979323846;
 	/** pi/2 */
-	static const double PI_2;
+	static const double PI_2      = 1.57079632679489661923;
 	/** pi/4 */
-	static const double PI_4;
+	static const double PI_4      = 0.78539816339744830962;
 	/** 1/pi */
-	static const double _1_PI;
+	static const double _1_PI     = 0.31830988618379067154;
 	/** 2/pi */
-	static const double _2_PI;
+	static const double _2_PI     = 0.63661977236758134308;
 	/** 2/sqrt(pi)*/
-	static const double _2_SQRTPI;
+	static const double _2_SQRTPI = 1.12837916709551257390;
 	/** sqrt(2) */
-	static const double SQRT2;
+	static const double SQRT2     = 1.41421356237309504880;
 	/** sqrt(1/2) */
-	static const double SQRT1_2;
+	static const double SQRT1_2   = 0.70710678118654752440;
 
     /**     
      * Converts a float to an integer using a processor instruction. 
@@ -91,21 +91,6 @@ public:
         return FastMathSin(angle);
     }
 };
-
-// Math constants
-const double FastMath::E        = 2.7182818284590452354;
-const double FastMath::LOG2E    = 1.4426950408889634074;
-const double FastMath::LOG10E   = 0.43429448190325182765;
-const double FastMath::LN2      = 0.69314718055994530942;
-const double FastMath::LN10     = 2.30258509299404568402;
-const double FastMath::PI       = 3.14159265358979323846;
-const double FastMath::PI_2     = 1.57079632679489661923;
-const double FastMath::PI_4     = 0.78539816339744830962;
-const double FastMath::_1_PI     = 0.31830988618379067154;
-const double FastMath::_2_PI     = 0.63661977236758134308;
-const double FastMath::_2_SQRTPI = 1.12837916709551257390;
-const double FastMath::SQRT2    = 1.41421356237309504880;
-const double FastMath::SQRT1_2  = 0.70710678118654752440;
 
 #endif
 
