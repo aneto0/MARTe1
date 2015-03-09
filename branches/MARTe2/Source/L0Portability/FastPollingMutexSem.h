77dd2c57dd2826e5d212fd9de7dc434920e945db
gferro <gferro@428a0d1b-0fa9-4229-b4c5-a2ab7cbf0508>
2015-03-09 18:14:59 +0000

    add FastPollingMutexSem in level0

--- branches/MARTe2/Source/L0Portability/FastPollingMutexSem.h
+++ branches/MARTe2/Source/L0Portability/FastPollingMutexSem.h
@@ -2,23 +2,23 @@
  * Copyright 2011 EFDA | European Fusion Development Agreement
  *
  * Licensed under the EUPL, Version 1.1 or - as soon they 
- will be approved by the European Commission - subsequent
- versions of the EUPL (the "Licence");
+ will be approved by the European Commission - subsequent  
+ versions of the EUPL (the "Licence"); 
  * You may not use this work except in compliance with the 
- Licence.
+ Licence. 
  * You may obtain a copy of the Licence at: 
  *  
  * http://ec.europa.eu/idabc/eupl
  *
  * Unless required by applicable law or agreed to in 
- writing, software distributed under the Licence is
- distributed on an "AS IS" basis,
+ writing, software distributed under the Licence is 
+ distributed on an "AS IS" basis, 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
- express or implied.
+ express or implied. 
  * See the Licence for the specific language governing 
- permissions and limitations under the Licence.
+ permissions and limitations under the Licence. 
  *
- * $Id: FastPollingMutexSem.h 3 2012-01-15 16:26:07Z aneto $
+ * $Id:$
  *
  **/
 
@@ -67,8 +67,8 @@ public:
     /** Locks Mux returns False if locking failed. Waits polling at a max rate of a msec
      Timeout is the minimum Timeout. The actual time can be much bigger    */
     inline bool FastLock(TimeoutType msecTimeout = TTInfiniteWait) {
-        int64 ticksStop = msecTimeout.HighResolutionTimerTicks()();
-        ticksStop += HighResolutionTimer::Counter()();
+        int64 ticksStop = msecTimeout.HighResolutionTimerTicks();
+        ticksStop += HighResolutionTimer::Counter();
         while (!Atomic::TestAndSet((int32 *) &flag)) {
             if (msecTimeout != TTInfiniteWait) {
                 int64 ticks = HighResolutionTimer::Counter();
