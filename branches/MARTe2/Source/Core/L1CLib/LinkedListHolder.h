74ae8dbe8ae67fcdc30fe1b821e960e1f8070bdb
gferro <gferro@428a0d1b-0fa9-4229-b4c5-a2ab7cbf0508>
2015-03-17 17:27:45 +0000

    Added controls of NULL argument in ListAdd() ListAddL() and ListExtract() functions.

--- branches/MARTe2/Source/Core/L1CLib/LinkedListHolder.h
+++ branches/MARTe2/Source/Core/L1CLib/LinkedListHolder.h
@@ -35,18 +35,18 @@
 class LinkedListHolder {
 protected:
     /** */
-    LinkedListable  llhRoot;
+    LinkedListable llhRoot;
 
     /** */
-    uint32          llhSize;
+    uint32 llhSize;
 
 public:
 
     /** will deallocate all the contents */
-    void CleanUp(){
+    void CleanUp() {
         LinkedListable *p = llhRoot.next;
         llhRoot.next = NULL;
-        while(p != NULL){
+        while (p != NULL) {
             LinkedListable *q = p;
             p = p->Next();
             delete q;
@@ -54,73 +54,88 @@ public:
         llhSize = 0;
     }
 
-
     /** does the equivalent of initialization: sets 0 to llhSize and NULL
-        to llhRoot->next */
-    void Reset(){
+     to llhRoot->next */
+    void Reset() {
         llhRoot.next = NULL;
         llhSize = 0;
     }
 
     /** */
-    LinkedListHolder(){
+    LinkedListHolder() {
         llhSize = 0;
         llhRoot.next = NULL;
     }
 
     /**  will deallocate all the
-         contents providing all the members have virtual destructors */
-    virtual ~LinkedListHolder(){
+     contents providing all the members have virtual destructors */
+    virtual ~LinkedListHolder() {
         CleanUp();
     }
 
     /**  return the first element */
-    LinkedListable *List() const{
+    LinkedListable *List() const {
         return llhRoot.Next();
-    };
+    }
+    ;
 
     /**  return the first element */
-    uint32 ListSize()const {
+    uint32 ListSize() const {
         return llhSize;
-    };
+    }
+    ;
 
     /**  insert in the first location the element p */
-    inline void FastListInsertSingle(LinkedListable &p){
+    inline void FastListInsertSingle(LinkedListable &p) {
         llhSize++;
         p.next = llhRoot.next;
         llhRoot.next = &p;
     }
 
     /**  insert in the first location the list p */
-    void ListInsert(LinkedListable *p){
+    void ListInsert(LinkedListable *p) {
+        if (p == NULL) {
+            return;
+        }
         llhSize += p->Size();
         llhRoot.Insert(p);
     }
 
     /**  insert in the right location the list p using sorting sorter */
-    void ListInsert(LinkedListable *p,SortFilter *sorter){
+    void ListInsert(LinkedListable *p, SortFilter *sorter) {
+        if (p == NULL) {
+            return;
+        }
         llhSize += p->Size();
-        llhRoot.Insert(p,sorter);
+        llhRoot.Insert(p, sorter);
     }
 
     /**  insert in the right location the list p using sorting sorter */
-    void ListInsert(LinkedListable *p,SortFilterFn *sorter){
+    void ListInsert(LinkedListable *p, SortFilterFn *sorter) {
+        if (p == NULL) {
+            return;
+        }
         llhSize += p->Size();
-        llhRoot.Insert(p,sorter);
+        llhRoot.Insert(p, sorter);
     }
 
     /**  inserts as the n-th element of list (if possible otherwise as bottom) */
-    void ListInsert(LinkedListable *q,uint32 index){
-        if (q == NULL) return;
+    void ListInsert(LinkedListable *q, uint32 index) {
+        if (q == NULL)
+            return;
         LinkedListable *p = &llhRoot;
-        while ((p->next != NULL) && (index > 0)){ p = p->next; index--; }
+        while ((p->next != NULL) && (index > 0)) {
+            p = p->next;
+            index--;
+        }
         llhSize += q->Size();
         p->Insert(q);
     }
 
     /**  insert in the first location the list p */
-    void ListInsertL(LinkedListHolder *p){
-        if (p == NULL) return;
+    void ListInsertL(LinkedListHolder *p) {
+        if (p == NULL)
+            return;
         llhSize += p->ListSize();
         llhRoot.Insert(p->List());
         p->llhRoot.next = NULL;
@@ -128,167 +143,195 @@ public:
     }
 
     /**  insert in the right location the list p using sorting sorter */
-    void ListInsertL(LinkedListHolder *p,SortFilter *sorter){
-        if (p == NULL) return;
+    void ListInsertL(LinkedListHolder *p, SortFilter *sorter) {
+        if (p == NULL)
+            return;
         llhSize += p->ListSize();
-        llhRoot.Insert(p->List(),sorter);
+        llhRoot.Insert(p->List(), sorter);
         p->llhRoot.next = NULL;
         p->llhSize = 0;
     }
 
     /**  insert in the right location the list p using sorting sorter */
-    void ListInsertL(LinkedListHolder *p,SortFilterFn *sorter){
-        if (p == NULL) return;
+    void ListInsertL(LinkedListHolder *p, SortFilterFn *sorter) {
+        if (p == NULL)
+            return;
         llhSize += p->ListSize();
-        llhRoot.Insert(p->List(),sorter);
+        llhRoot.Insert(p->List(), sorter);
         p->llhRoot.next = NULL;
         p->llhSize = 0;
     }
 
     /**  inserts as the n-th element of list (if possible otherwise as bottom) */
-    void ListInsertL(LinkedListHolder *p,uint32 index){
-        if (p == NULL) return;
-        ListInsert(p->llhRoot.next,index);
+    void ListInsertL(LinkedListHolder *p, uint32 index) {
+        if (p == NULL)
+            return;
+        ListInsert(p->llhRoot.next, index);
         p->llhRoot.next = NULL;
         p->llhSize = 0;
     }
 
     /**  add an element at the end of the list */
-    void ListAdd(LinkedListable *p){
-        llhSize ++;
+    void ListAdd(LinkedListable *p) {
+        if (p == NULL) {
+            return;
+        }
+        llhSize++;
         llhRoot.Add(p);
     }
 
     /**  add an element at the end of the list */
-    void ListAddL(LinkedListable *p){
+    void ListAddL(LinkedListable *p) {
+        if (p == NULL) {
+            return;
+        }
         llhSize += p->Size();
         llhRoot.AddL(p);
     }
 
     /**  search if p is a member */
-    bool ListSearch(LinkedListable *p){
-        if (llhRoot.Next() == NULL) return False;
+    bool ListSearch(LinkedListable *p) {
+        if (llhRoot.Next() == NULL)
+            return False;
         return llhRoot.Next()->Search(p);
     }
 
     /**  search an element using a specific criteria */
-    LinkedListable *ListSearch(SearchFilter *filter){
-        if (llhRoot.Next() == NULL) return NULL;
+    LinkedListable *ListSearch(SearchFilter *filter) {
+        if (llhRoot.Next() == NULL)
+            return NULL;
         return llhRoot.Next()->Search(filter);
     }
 
     /**  search an element using a specific criteria */
-    LinkedListable *ListSearch(SearchFilterFn *filter){
-        if (llhRoot.Next() == NULL) return NULL;
+    LinkedListable *ListSearch(SearchFilterFn *filter) {
+        if (llhRoot.Next() == NULL)
+            return NULL;
         return llhRoot.Next()->Search(filter);
     }
 
     /**  remove the requested element from the list.
-         start searching from next element */
-    bool ListExtract(LinkedListable *p){
-        if (llhRoot.Extract(p) == True){
-            llhSize --;
+     start searching from next element */
+    bool ListExtract(LinkedListable *p) {
+        if (llhRoot.Extract(p) == True) {
+            llhSize--;
             return True;
-        } else return False;
+        }
+        else
+            return False;
     }
 
     /**  find and remove one element from list using filter as criteria */
-    LinkedListable *ListExtract(SearchFilter *filter){
+    LinkedListable *ListExtract(SearchFilter *filter) {
         LinkedListable *p = llhRoot.Extract(filter);
-        if (p != NULL) llhSize --;
+        if (p != NULL)
+            llhSize--;
         return p;
     }
 
     /**  find and remove one element from list using filter */
-    LinkedListable *ListExtract(SearchFilterFn *filter){
+    LinkedListable *ListExtract(SearchFilterFn *filter) {
         LinkedListable *p = llhRoot.Extract(filter);
-        if (p != NULL) llhSize --;
+        if (p != NULL)
+            llhSize--;
         return p;
     }
 
     /**  delete the requested element. start searching from next element */
-    bool ListDelete(LinkedListable *p){
-        if (llhRoot.Delete(p) == True){
-            llhSize --;
+    bool ListDelete(LinkedListable *p) {
+        if (llhRoot.Delete(p) == True) {
+            llhSize--;
             return True;
-        } else return False;
+        }
+        else
+            return False;
     }
 
     /**  delete an element using a specific criteria */
-    bool ListDelete(SearchFilter *filter){
+    bool ListDelete(SearchFilter *filter) {
         uint32 deleted = llhRoot.Delete(filter);
         llhSize -= deleted;
-        return (deleted !=0);
+        return (deleted != 0);
     }
 
     /** delete an element using a specific criteria.
-        Safe from reentrance from destructor of object
-       (complex graph destruction) */
-    bool ListSafeDelete(SearchFilter *filter){
-        if (filter == NULL) return False;
+     Safe from reentrance from destructor of object
+     (complex graph destruction) */
+    bool ListSafeDelete(SearchFilter *filter) {
+        if (filter == NULL)
+            return False;
         int deleted = 0;
         LinkedListable *p = List();
-        while(p != NULL){
-            if (filter->Test(p)){
-                if (ListExtract(p))  {
+        while (p != NULL) {
+            if (filter->Test(p)) {
+                if (ListExtract(p)) {
                     delete p;
                     // p->next may not be a safe pointer so assume the worst and start over again
                     p = List();
                     deleted++;
-                } else p = p->next;
-            } else p = p->next;
+                }
+                else
+                    p = p->next;
+            }
+            else
+                p = p->next;
         }
         return (deleted != 0);
     }
 
     /**  delete an element using a specific criteria */
-    bool ListDelete(SearchFilterFn *filter){
+    bool ListDelete(SearchFilterFn *filter) {
         uint32 deleted = llhRoot.Delete(filter);
         llhSize -= deleted;
-        return (deleted !=0);
+        return (deleted != 0);
     }
 
-
     /**  Bubble Sort the sub-list to the right of this element */
-    void ListBSort(SortFilter *sorter){
+    void ListBSort(SortFilter *sorter) {
         llhRoot.BSort(sorter);
     }
 
     /**  Bubble Sort the sub-list to the right of this element */
-    void ListBSort(SortFilterFn *sorter){
+    void ListBSort(SortFilterFn *sorter) {
         llhRoot.BSort(sorter);
     }
 
     /**  looks into the list in an ordered fashion: index = 0 is the top */
-    LinkedListable *ListPeek(uint32 index){
+    LinkedListable *ListPeek(uint32 index) {
         return llhRoot.Next()->Peek(index);
     }
 
     /**  removes and Gets the n-th element of the list */
-    LinkedListable *ListExtract(uint32 index=0){
+    LinkedListable *ListExtract(uint32 index = 0) {
         LinkedListable *p = &llhRoot;
-        while ((p != NULL) && (index > 0)){ p = p->next; index--; }
+        while ((p != NULL) && (index > 0)) {
+            p = p->next;
+            index--;
+        }
+        if (p == NULL)
+            return NULL;
         LinkedListable *q = p->next;
-        if (q != NULL){
+        if (q != NULL) {
             llhSize--;
             p->next = q->next;
         }
-        if (q != NULL) q->next = NULL;
+        if (q != NULL)
+            q->next = NULL;
         return q;
     }
 
     /**  for each item in the list do <it> */
-    void ListIterate(Iterator *it){
-        if (llhRoot.Next() != NULL) llhRoot.Next()->Iterate(it);
+    void ListIterate(Iterator *it) {
+        if (llhRoot.Next() != NULL)
+            llhRoot.Next()->Iterate(it);
     }
 
     /**  for each item in the list do <it> */
-    void ListIterate(IteratorFn *it){
-        if (llhRoot.Next() != NULL) llhRoot.Next()->Iterate(it);
+    void ListIterate(IteratorFn *it) {
+        if (llhRoot.Next() != NULL)
+            llhRoot.Next()->Iterate(it);
     }
 
 };
-
-
 
 #endif
