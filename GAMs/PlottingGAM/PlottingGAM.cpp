f556593c3f7fa4b0801ab470d746e75d2ba03238
Giuseppe Ferrò <wergrest@hotmail.it>
2015-03-25 15:21:21 +0000

    Add statistic mode and Histogram plot mode. Adjust PlottingGam.js to work with the new version of RGraph libraries.

--- GAMs/PlottingGAM/PlottingGAM.cpp
+++ GAMs/PlottingGAM/PlottingGAM.cpp
@@ -2,25 +2,25 @@
  * Copyright 2011 EFDA | European Fusion Development Agreement
  *
  * Licensed under the EUPL, Version 1.1 or - as soon they 
-   will be approved by the European Commission - subsequent  
-   versions of the EUPL (the "Licence"); 
+ will be approved by the European Commission - subsequent  
+ versions of the EUPL (the "Licence"); 
  * You may not use this work except in compliance with the 
-   Licence. 
+ Licence. 
  * You may obtain a copy of the Licence at: 
  *  
  * http://ec.europa.eu/idabc/eupl
  *
  * Unless required by applicable law or agreed to in 
-   writing, software distributed under the Licence is 
-   distributed on an "AS IS" basis, 
+ writing, software distributed under the Licence is 
+ distributed on an "AS IS" basis, 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either 
-   express or implied. 
+ express or implied. 
  * See the Licence for the specific language governing 
-   permissions and limitations under the Licence. 
+ permissions and limitations under the Licence. 
  *
  * $Id$
  *
-**/
+ **/
 
 #include "PlottingGAM.h"
 #include "CDBExtended.h"
@@ -34,19 +34,27 @@ bool PlottingGAM::Initialise(ConfigurationDataBase &cdbData) {
 
     numberOfPlotWindows = Size();
 
-    if((gcrtpw = new GCRTemplate<PlotWindow>[numberOfPlotWindows]) == NULL) {
-	AssertErrorCondition(InitialisationError, "PlottingGAM::Initialise: %s unable to allocate memory for reference storage", Name());
-	return False;
+    if ((gcrtpw = new GCRTemplate< PlotWindow > [numberOfPlotWindows])
+            == NULL) {
+        AssertErrorCondition(
+                InitialisationError,
+                "PlottingGAM::Initialise: %s unable to allocate memory for reference storage",
+                Name());
+        return False;
     }
 
-    for(int i = 0 ; i < numberOfPlotWindows ; i++) {
-    	GCRTemplate<PlotWindow> gcr = Find(i);
-    	if(gcr.IsValid()) {
-    	    gcrtpw[i] = gcr;
-    	} else {
-	    AssertErrorCondition(InitialisationError, "PlottingGAM::Initialise: %s only PlotWindow objects allowed", Name());
-	    return False;
-	}
+    for (int i = 0; i < numberOfPlotWindows; i++) {
+        GCRTemplate< PlotWindow > gcr = Find(i);
+        if (gcr.IsValid()) {
+            gcrtpw[i] = gcr;
+        }
+        else {
+            AssertErrorCondition(
+                    InitialisationError,
+                    "PlottingGAM::Initialise: %s only PlotWindow objects allowed",
+                    Name());
+            return False;
+        }
     }
 
     return True;
@@ -55,8 +63,8 @@ bool PlottingGAM::Initialise(ConfigurationDataBase &cdbData) {
 /// Called in every control loop
 bool PlottingGAM::Execute(GAM_FunctionNumbers execFlag) {
 
-    for(int i = 0 ; i < numberOfPlotWindows ; i++) {
-    	gcrtpw[i]->Execute(execFlag);
+    for (int i = 0; i < numberOfPlotWindows; i++) {
+        gcrtpw[i]->Execute(execFlag);
     }
 
     return True;
@@ -70,30 +78,30 @@ bool PlottingGAM::ProcessHttpMessage(HttpStream &hStream) {
     ajaxString.Printf("var titles = new Array(%d);\n", numberOfPlotWindows);
     ajaxString.Printf("var xLabels = new Array(%d);\n", numberOfPlotWindows);
     ajaxString.Printf("var yLabels = new Array(%d);\n", numberOfPlotWindows);
-    ajaxString.Printf("var xAxisLimits = new Array(%d);\n", numberOfPlotWindows);
-	int i;
-    for(i = 0 ; i < numberOfPlotWindows ; i++) {
-    	gcrtpw[i]->AppendJsDataString(ajaxString);
-    	ajaxString.Printf("data[%d] = pwData;\n", i);
-    	ajaxString.Printf("titles[%d] = title;\n", i);
-    	ajaxString.Printf("xLabels[%d] = xLabel;\n", i);
-    	ajaxString.Printf("yLabels[%d] = yLabel;\n", i);
-    	ajaxString.Printf("xAxisLimits[%d] = xLimits;\n", i);
-    }    
-    
+    ajaxString.Printf("var xAxisLimits = new Array(%d);\n",
+                      numberOfPlotWindows);
+    int i;
+    for (i = 0; i < numberOfPlotWindows; i++) {
+        gcrtpw[i]->AppendJsDataString(ajaxString);
+        ajaxString.Printf("data[%d] = pwData;\n", i);
+        ajaxString.Printf("titles[%d] = title;\n", i);
+        ajaxString.Printf("xLabels[%d] = xLabel;\n", i);
+        ajaxString.Printf("yLabels[%d] = yLabel;\n", i);
+        ajaxString.Printf("xAxisLimits[%d] = xLimits;\n", i);
+    }
+
     FString refreshData;
     refreshData.SetSize(0);
     hStream.Seek(0);
-    if(hStream.Switch("InputCommands.PlottingGAMRefreshData")){
+    if (hStream.Switch("InputCommands.PlottingGAMRefreshData")) {
         hStream.Seek(0);
         hStream.GetToken(refreshData, "");
-        hStream.Switch((uint32)0);
-    	hStream.Printf("%s\n", ajaxString.Buffer());
-    	hStream.WriteReplyHeader(True);
-    	return True;
+        hStream.Switch((uint32) 0);
+        hStream.Printf("%s\n", ajaxString.Buffer());
+        hStream.WriteReplyHeader(True);
+        return True;
     }
-
-    hStream.SSPrintf("OutputHttpOtions.Content-Type","text/html");
+    hStream.SSPrintf("OutputHttpOtions.Content-Type", "text/html");
     hStream.keepAlive = False;
 
     //copy to the client
@@ -109,10 +117,25 @@ bool PlottingGAM::ProcessHttpMessage(HttpStream &hStream) {
     hStream.Printf("<title>Plotting GAM</title>\n");
 
     /* Include relevant RGraph libraries */
-    hStream.Printf("<script src=\"/RGRAPH_LIB_DIR/RGraph.common.core.js\"></script>\n");
-    hStream.Printf("<script src=\"/RGRAPH_LIB_DIR/RGraph.common.zoom.js\"></script>\n");
-    hStream.Printf("<script src=\"/RGRAPH_LIB_DIR/RGraph.line.js\"></script>\n");
-    hStream.Printf("<script src=\"/PLOTTING_GAM_DIR/PlottingGAM.js\"></script>\n");
+    hStream.Printf(
+            "<script src=\"/RGRAPH_LIB_DIR/RGraph.common.core.js\"></script>\n");
+    hStream.Printf(
+            "<script src=\"/RGRAPH_LIB_DIR/RGraph.common.zoom.js\"></script>\n");
+    hStream.Printf(
+            "<script src=\"/RGRAPH_LIB_DIR/RGraph.line.js\"></script>\n");
+    hStream.Printf("<script src=\"/RGRAPH_LIB_DIR/RGraph.bar.js\"></script>\n");
+
+    /* Include the js file to plot in line mode */
+
+        hStream.Printf(
+                "<script src=\"/PLOTTING_GAM_DIR/PlottingGAM.js\"></script>\n");
+
+    /* Include the js file to plot in histogram mode */
+        hStream.Printf(
+                "<script src=\"/PLOTTING_GAM_DIR/HistogramBar.js\"></script>\n");
+
+    hStream.Printf(
+            "<script src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>\n");
 
     /* End page head */
     hStream.Printf("</head>\n");
@@ -127,7 +150,8 @@ bool PlottingGAM::ProcessHttpMessage(HttpStream &hStream) {
     hStream.Printf("request.open('GET', url, true);\n");
     hStream.Printf("request.onreadystatechange = plotManager;\n");
     hStream.Printf("request.send(null);\n");
-    hStream.Printf("window.setTimeout(timedUpdate, %d);\n", httpRefreshMsecTime);
+    hStream.Printf("window.setTimeout(timedUpdate, %d);\n",
+                   httpRefreshMsecTime);
     hStream.Printf("}\n");
 
     hStream.Printf("function plotManager() {\n");
@@ -135,23 +159,41 @@ bool PlottingGAM::ProcessHttpMessage(HttpStream &hStream) {
     hStream.Printf("return;\n");
     hStream.Printf("}\n");
     hStream.Printf("eval(request.responseText);\n");
-    for(i = 0 ; i < numberOfPlotWindows ; i++) {
-	hStream.Printf("plot('canvas%d', data[%d], xAxisLimits[%d], titles[%d], xLabels[%d], yLabels[%d], %d);\n", i, i, i, i, i, i, gcrtpw[i]->onlyPositiveYBool);
+    for (i = 0; i < numberOfPlotWindows; i++) {
+        //plot in line mode for each object initialized in line mode.
+	if (gcrtpw[i]->plotType == "Line"){
+        	hStream.Printf(
+        	        "plotLine('canvas%d', data[%d], xAxisLimits[%d], titles[%d], xLabels[%d], yLabels[%d], %d, %f);\n",
+        	        i, i, i, i, i, i, gcrtpw[i]->onlyPositiveYBool, gcrtpw[i]->zoom);
+	}
+	//plot in bar mode for each object initialized in bar mode.
+	if (gcrtpw[i]->plotType == "Bar"){
+		  hStream.Printf(
+        	        "plotBar('canvas%d', data[%d], xAxisLimits[%d], titles[%d], xLabels[%d], yLabels[%d], %d, %f);\n",
+        	        i, i, i, i, i, i, gcrtpw[i]->onlyPositiveYBool, gcrtpw[i]->zoom);
+	}
     }
     hStream.Printf("}\n");
-    
+
     hStream.Printf("</script>\n");
-    
+
     int32 heightCumulative = 0;
-    for(i = 0 ; i < numberOfPlotWindows ; i++) {
-    	hStream.Printf("<canvas id='canvas%dtitle' width='%d' height='50' style='position:absolute; left:0px; top:%dpx'></canvas>\n", i, gcrtpw[i]->plotPixelWidth, heightCumulative);
-	heightCumulative += 50;
-    	hStream.Printf("<canvas id='canvas%d' width='%d' height='%d' style='position:absolute; left:0px; top:%dpx'></canvas>\n", i, gcrtpw[i]->plotPixelWidth, gcrtpw[i]->plotPixelHeight, heightCumulative);
-	heightCumulative += gcrtpw[i]->plotPixelHeight;
+    for (i = 0; i < numberOfPlotWindows; i++) {
+        hStream.Printf(
+                "<canvas id='canvas%dtitle' width='%d' height='50' style='position:absolute; left:0px; top:%dpx'></canvas>\n",
+                i, gcrtpw[i]->plotPixelWidth, heightCumulative);
+        heightCumulative += 50;
+        hStream.Printf(
+                "<canvas id='canvas%d' width='%d' height='%d' style='position:absolute; left:0px; top:%dpx'></canvas>\n",
+                i, gcrtpw[i]->plotPixelWidth, gcrtpw[i]->plotPixelHeight,
+                heightCumulative);
+        heightCumulative += gcrtpw[i]->plotPixelHeight;
     }
-    hStream.Printf("<canvas id='canvasAck' width='400' height='400' style='position:absolute; left:0px; top:%dpx'></canvas>\n", heightCumulative+50);
+    hStream.Printf(
+            "<canvas id='canvasAck' width='400' height='400' style='position:absolute; left:0px; top:%dpx'></canvas>\n",
+            heightCumulative + 50);
     hStream.Printf("<script>\n");
-    hStream.Printf("putRGraphLabel(\"canvasAck\");\n", numberOfPlotWindows-1);
+    hStream.Printf("putRGraphLabel(\"canvasAck\");\n", numberOfPlotWindows - 1);
     hStream.Printf("</script>\n");
     hStream.Printf("</body>\n");
 
@@ -159,4 +201,5 @@ bool PlottingGAM::ProcessHttpMessage(HttpStream &hStream) {
 
     return True;
 }
-OBJECTLOADREGISTER(PlottingGAM, "$Id$")
+OBJECTLOADREGISTER(PlottingGAM,
+                   "$Id$")
